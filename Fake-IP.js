78a1e3998c548ea6d3a67a404a5b38c6616f2dff
792825013 <792825013@qq.com>
2025-02-28 19:25:42 +0800

    Update Fake-IP.js

--- Fake-IP.js
+++ Fake-IP.js
@@ -4,16 +4,18 @@ const BASE_CONFIG = {
     enable: true,
     listen: '0.0.0.0:53',
     'enhanced-mode': 'fake-ip',
+    'fake-ip-range': '198.18.0.1/16', // 防止IP冲突
     'nameserver-policy': {
       'geosite:cn': ['https://223.5.5.5/dns-query', 'https://doh.pub/dns-query'],
       'geosite:geolocation-!cn': ['https://1.1.1.1/dns-query', 'https://dns.google/dns-query']
-    }
+    },
+    'default-nameserver': ['223.5.5.5', '119.29.29.29'] // 备用DNS
   },
   rules: ['GEOIP,private,DIRECT,no-resolve', 'GEOSITE,cn,DIRECT', 'GEOIP,cn,DIRECT,no-resolve', 'MATCH,GLOBAL'],
   'proxy-groups': [
     { name: 'GLOBAL', type: 'select', proxies: ['SG新加坡', 'JP日本'] },
-    { name: 'SG新加坡', type: 'url-test', proxies: [] },
-    { name: 'JP日本', type: 'url-test', proxies: [] }
+    { name: 'SG新加坡', type: 'url-test', proxies: [], url: 'http://www.gstatic.com/generate_204', interval: 300, tolerance: 50 },
+    { name: 'JP日本', type: 'url-test', proxies: [], url: 'http://www.gstatic.com/generate_204', interval: 300, tolerance: 50 }
   ]
 };
 
