9b40eb6959af38cc0bc6f1ab8af3a850c6fc97d6
792825013 <792825013@qq.com>
2025-03-01 21:50:40 +0800

    Update Fake-IP.js

--- Fake-IP.js
+++ Fake-IP.js
@@ -2,37 +2,37 @@ const BASE_CONFIG = {
   mode: 'rule',
   dns: {
     enable: true,
-    listen: '0.0.0.0:53', // 监听标准 DNS 端口
-    'enhanced-mode': 'fake-ip', // 使用虚拟 IP 防泄露
-    'fake-ip-range': '198.18.0.1/16', // 防止 IP 冲突
-    'fake-ip-filter': ['*.lan', '*.localdomain', '+.home.arpa'], // 过滤本地域名
+    listen: '0.0.0.0:53',
+    'enhanced-mode': 'fake-ip',
+    'fake-ip-range': '198.18.0.1/16',
+    'fake-ip-filter': ['*.lan', '*.localdomain', '+.home.arpa'],
     'nameserver-policy': {
-      'geosite:cn': ['https://223.5.5.5/dns-query', 'https://120.53.53.53/dns-query'], // 国内用阿里和快手 DoH
-      'geosite:geolocation-!cn': ['https://1.1.1.1/dns-query', 'https://8.8.8.8/dns-query'] // 国外用 Cloudflare 和 Google DoH
+      'geosite:cn': ['https://223.5.5.5/dns-query', 'https://120.53.53.53/dns-query'],
+      'geosite:geolocation-!cn': ['https://1.1.1.1/dns-query', 'https://8.8.8.8/dns-query']
     },
-    'default-nameserver': ['223.5.5.5'] // 单一备用 DNS
+    'default-nameserver': ['223.5.5.5']
+  },
+  tun: {
+    enable: true,
+    stack: 'system',
+    'dns-hijack': ['any:53']
   },
   rules: [
-    'GEOIP,private,DIRECT,no-resolve', // 私有网络直连
-    'GEOSITE,cn,DIRECT', // 国内网站直连
-    'MATCH,GLOBAL' // 其他走代理
+    'GEOIP,private,DIRECT,no-resolve',
+    'GEOSITE,cn,DIRECT',
+    'MATCH,GLOBAL'
   ],
   'proxy-groups': [
-    { name: 'GLOBAL', type: 'select', proxies: ['SG新加坡', 'JP日本'] }, // 主选择组
-    { name: 'SG新加坡', type: 'url-test', proxies: [], url: 'http://www.gstatic.com/generate_204', interval: 300, tolerance: 50 }, // 新加坡自动选择
-    { name: 'JP日本', type: 'url-test', proxies: [], url: 'http://www.gstatic.com/generate_204', interval: 300, tolerance: 50 } // 日本自动选择
+    { name: 'GLOBAL', type: 'url-test', proxies: [], url: 'http://www.gstatic.com/generate_204', interval: 300, tolerance: 50 }
   ]
 };
 
 function main(config) {
-  const sgProxies = BASE_CONFIG['proxy-groups'][1].proxies;
-  const jpProxies = BASE_CONFIG['proxy-groups'][2].proxies;
-  const sgRegex = /(新加坡|🇸🇬)/; // 正则匹配新加坡
-  const jpRegex = /(日本|🇯🇵)/; // 正则匹配日本
+  const globalProxies = BASE_CONFIG['proxy-groups'][0].proxies;
+  const sgRegex = /(新加坡|🇸🇬)/;
 
   config.proxies.forEach(({ name }) => {
-    if (sgRegex.test(name)) sgProxies.push(name);
-    else if (jpRegex.test(name)) jpProxies.push(name);
+    if (sgRegex.test(name)) globalProxies.push(name);
   });
 
   return { ...config, ...BASE_CONFIG };
